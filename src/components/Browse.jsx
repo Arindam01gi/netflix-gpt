be5165bdd3bd92ff2831019402093a0b3dd9ba4e
arindam01gi <arindammaiti2018@gmail.com>
2025-07-21 19:01:19 +0530

    Getting API data from TMDB API done

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './Header'
+import UseNowPlayingMovies from '../hooks/useNowPlayingMovies'
+
 
 
 const Browse = () => {
+ UseNowPlayingMovies() 
+
+
   return (
-    <div className="mt-12"> 
+    <div className="mt-12">
       <div className="flex items-center justify-between w-full">
         <Header />
       </div>
---
+++ src/hooks/UseNowPlayingMovies.tsx
@@ -0,0 +1,27 @@
+import { API_OPTIONS } from '../utils/constant'
+import { useDispatch } from 'react-redux'
+import { addNowPlayingMovie } from '../store/movieSlice'
+import { useEffect } from 'react';
+
+const UseNowPlayingMovies = () =>{
+    const dispatch = useDispatch();
+
+
+    const getNowPlayingMoviesList = async () => {
+      const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', 
+        API_OPTIONS
+      )
+      const json = await data.json()
+      console.log(json.results)
+      dispatch(addNowPlayingMovie(json.results))
+    }
+  
+  
+    useEffect(() =>{
+      getNowPlayingMoviesList();
+    },[])
+  
+  
+}
+
+export default UseNowPlayingMovies;
