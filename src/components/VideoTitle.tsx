efd40c236bce799aeca4ad4f321f2c74a439ab41
arindam01gi <arindammaiti2018@gmail.com>
2025-07-22 21:18:54 +0530

    update

--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -7,7 +7,7 @@ const VideoTitle = ({ original_title, overview }: { original_title: string, over
             <p className='text-lg  px-36 w-3/4 pt-6'>{overview}</p>
             <div className='px-36 pt-6 gap-4 flex '>
                 <button className='bg-gray-200 px-12 py-3 text-lg font-semibold rounded-md shadow-md '> <i className="fa-solid fa-play mr-3"></i>Play</button>
-                <button className='bg-gray-700 px-12 py-3 text-lg font-semibold rounded-md shadow-md text-white opacity-50 '> <i className="fa-solid fa-play mr-3"></i>More info</button>
+                <button className='bg-gray-500 px-8 py-3 text-lg rounded-md shadow-md text-white opacity-80 '> <i className="fa fa-circle-info mr-3 "></i>More info</button>
             </div>
         </div>
     )
