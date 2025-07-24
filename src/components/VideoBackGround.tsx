289a431543c72e0f628f6f1a0a1c6455cb94fe6f
arindam01gi <arindammaiti2018@gmail.com>
2025-07-24 18:35:08 +0530

    main container completed

--- src/components/VideoBackGround.tsx
+++ src/components/VideoBackGround.tsx
@@ -7,10 +7,10 @@ const VideoBackGround = ({ movie_id }) => {
     const trailerVideo = useSelector((store: any) => store.movies?.trailerVideo)
     useMovieTrailer(movie_id)
     return (
-        <div className='overflow-x-hidden'>
+        <div className='overflow-x-hidden w-screen'>
             <iframe
             className='w-screen aspect-video'
-                src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=JZw06_bgTgjl4kQw`}
+                src={`https://www.youtube.com/embed/${trailerVideo?.key}?si=JZw06_bgTgjl4kQw&autoplay=1&mute=1&loop=1`}
                 title="YouTube video player"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             ></iframe>
--- src/components/VideoTitle.tsx
+++ src/components/VideoTitle.tsx
@@ -7,7 +7,10 @@ const VideoTitle = ({ original_title, overview }: { original_title: string, over
             <p className='text-lg  px-36 w-3/4 pt-6'>{overview}</p>
             <div className='px-36 pt-6 gap-4 flex '>
                 <button className='bg-white px-12 py-3 text-lg font-semibold rounded-md shadow-md  text-black hover:opacity-60 cursor-pointer'> <i className="fa-solid fa-play mr-3"></i>Play</button>
-                <button className='bg-gray-500 px-8 py-3 text-lg rounded-md shadow-md text-white opacity-80 '> <i className="fa fa-circle-info mr-3 "></i>More info</button>
+                <button className='bg-gray-500 px-8 py-3 text-lg rounded-md shadow-md text-white opacity-50 flex items-center '>
+                    <span className='mr-3'>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-info-icon lucide-info"><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg>
+                    </span> More info</button>
             </div>
         </div>
     )
