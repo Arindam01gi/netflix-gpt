5bd0b72e478becaf3be468570f3cc30e0d798e7e
arindam01gi <arindammaiti2018@gmail.com>
2025-09-22 18:03:42 +0530

    Update TVSecondaryContainer to display popular TV series; refactor usePopularMovies hook to fetch popular series data and dispatch to state management.

--- src/components/tv/TVSecondaryContainer.tsx
+++ src/components/tv/TVSecondaryContainer.tsx
@@ -5,13 +5,17 @@ import MovieList from '../MovieList'
 const TVSecondaryContainer = () => {
   const series = useSelector((store : any)=> store.tv)
   console.log("series",series)
+
   return (
     <div className='-mt-40 z-20'>
     <div className='overflow-x-hidden my-5'>
-      {series.arraivingTodayTvSeries && <MovieList title={"Arraiving Today"} movies= {series.arraivingTodayTvSeries}/>}
+      {series.arraivingTodayTvSeries && <MovieList title={"Arriving Today"} movies= {series.arraivingTodayTvSeries}/>}
     </div>
     <div className='overflow-x-hidden my-5'>
       {series.topRatedTvSeries && <MovieList title={"Top Rated"} movies= {series.topRatedTvSeries}/>}
+    </div>
+    <div className='overflow-x-hidden my-5'>
+      {series.popularTvSeries && <MovieList title={"Popular"} movies= {series.popularTvSeries}/>}
     </div>
     </div>
   )
--- src/hooks/usePopularMovies.tsx
+++ src/hooks/usePopularMovies.tsx
@@ -3,18 +3,19 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { API_OPTIONS } from '../utils/constant'
 import { addPopularMovies } from '../store/movieSlice'
+import { addPopularTvSeries } from '../store/tvSlice'
 
 const usePopularMovies = () => {
     const disptach = useDispatch();
 
-    const getPopularMovies = async () => {
-        const data = await fetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", API_OPTIONS)
+    const getPopularSeries = async () => {
+        const data = await fetch("https://api.themoviedb.org/3/tv/popular?language=en-US&page=1", API_OPTIONS)
         const json = await data.json()
-        disptach(addPopularMovies(json.results))
+        disptach(addPopularTvSeries(json.results))
     }
 
     useEffect(() => {
-        getPopularMovies()
+        getPopularSeries()
     }, [])
 
 }
